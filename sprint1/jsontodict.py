fc3cf85da07c57dbac6b878ae1b9f7e33b887853
Muneeb6225 <muneebk02@gmail.com>
2016-10-23 18:07:06 -0400

    Dictionary format for first planet of JSON
    Attributes to find:
    
    Inclination, declination, age, magK, magI, magR, magJ, magH, spectraltype, discoveryyear, planet temperature, planet distance, transittime, metallicity

--- sprint1/jsontodict.py
+++ sprint1/jsontodict.py
@@ -12,13 +12,16 @@ def readNASAExoplanetArchive():
     data = urllib.request.urlopen(url).read().decode('utf-8')
     a = json.loads(data)
     
-    # Formatted Dictionary
-    catalog = {}
-    # Loop through all exoplanets
-    for planet in range(0, len(a) - 1):
-	# Planet name is the key
-        planet_name = a[planet]["pl_hostname"]
-        del a[planet]["pl_hostname"]
-        catalog.update({planet_name: a[planet]})
-	
+    
+    planet_name = a[0]['pl_hostname']
+    space = ' '
+    final_name = planet_name + space + a[0]['pl_letter']
+    
+    
+    star = {'temperature': {'@errorplus': a[0]['st_tefferr1'], '@errorminus': a[0]['st_tefferr2'] }, 'name': planet_name, 'radius': {'@errorplus': a[0]['st_raderr1'], '@errorminus': a[0]['st_raderr2']}}
+    planet = {'Period': {'@errorplus': a[0]['pl_orbpererr1'], '@errorminus': a[0]['pl_orbpererr2'] }, 'lastupdate': a[0]['rowupdate'], 'name': final_name, 'semimajoraxis': {'@errorplus': a[0]['pl_orbsmaxerr1'], '@errorminus': a[0]['pl_orbsmaxerr2']}, 'radius': {'@errorplus': a[0]['pl_radjerr1'], '@errorminus': a[0]['pl_radjerr2']}, 'eccentricity': {'@errorplus': a[0]['pl_orbeccenerr1'], '@errorminus': a[0]['pl_orbeccenerr2'] } , 'discoverymethod': a[0]['pl_discmethod'], 'mass': {'@errorplus': a[0]['pl_bmassjerr1'], '@errorminus': a[0]['pl_bmassjerr2']}  }
+    
+    
+    catalog = { 'system' : {'name': planet_name, 'rightascension': a[0]['ra_str'], 'distance': {'@errorplus': a[0]['st_disterr1'], '@errorminus': a[0]['st_disterr2']}, 'star': star, 'planet' : planet} }
+    
     print(catalog)
