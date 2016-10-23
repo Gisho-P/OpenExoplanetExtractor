1861e3f6aa527715108b38b265eab6ea464087fb
Muneeb6225 <muneebk02@gmail.com>
2016-10-23 18:24:02 -0400

    Looped through all exoplanets with dictionary format
    - Need to add planet letter to planet name
    - Use Ordered Dictionary

--- sprint1/jsontodict.py
+++ sprint1/jsontodict.py
@@ -12,16 +12,17 @@ def readNASAExoplanetArchive():
     data = urllib.request.urlopen(url).read().decode('utf-8')
     a = json.loads(data)
     
+    final_catalog = {}
+    catalog = {}
+    for index in range(0, len(a) - 1):
+        
+        planet_name = a[index]['pl_hostname']
     
-    planet_name = a[0]['pl_hostname']
-    space = ' '
-    final_name = planet_name + space + a[0]['pl_letter']
+        star = {'temperature': {'@errorplus': a[index]['st_tefferr1'], '@errorminus': a[index]['st_tefferr2'] }, 'name': planet_name, 'radius': {'@errorplus': a[index]['st_raderr1'], '@errorminus': a[index]['st_raderr2']}}
+        planet = {'Period': {'@errorplus': a[index]['pl_orbpererr1'], '@errorminus': a[index]['pl_orbpererr2'] }, 'lastupdate': a[index]['rowupdate'], 'name': planet_name, 'semimajoraxis': {'@errorplus': a[index]['pl_orbsmaxerr1'], '@errorminus': a[index]['pl_orbsmaxerr2']}, 'radius': {'@errorplus': a[index]['pl_radjerr1'], '@errorminus': a[index]['pl_radjerr2']}, 'eccentricity': {'@errorplus': a[index]['pl_orbeccenerr1'], '@errorminus': a[index]['pl_orbeccenerr2'] } , 'discoverymethod': a[index]['pl_discmethod'], 'mass': {'@errorplus': a[index]['pl_bmassjerr1'], '@errorminus': a[index]['pl_bmassjerr2']}  }
     
     
-    star = {'temperature': {'@errorplus': a[0]['st_tefferr1'], '@errorminus': a[0]['st_tefferr2'] }, 'name': planet_name, 'radius': {'@errorplus': a[0]['st_raderr1'], '@errorminus': a[0]['st_raderr2']}}
-    planet = {'Period': {'@errorplus': a[0]['pl_orbpererr1'], '@errorminus': a[0]['pl_orbpererr2'] }, 'lastupdate': a[0]['rowupdate'], 'name': final_name, 'semimajoraxis': {'@errorplus': a[0]['pl_orbsmaxerr1'], '@errorminus': a[0]['pl_orbsmaxerr2']}, 'radius': {'@errorplus': a[0]['pl_radjerr1'], '@errorminus': a[0]['pl_radjerr2']}, 'eccentricity': {'@errorplus': a[0]['pl_orbeccenerr1'], '@errorminus': a[0]['pl_orbeccenerr2'] } , 'discoverymethod': a[0]['pl_discmethod'], 'mass': {'@errorplus': a[0]['pl_bmassjerr1'], '@errorminus': a[0]['pl_bmassjerr2']}  }
+        catalog.update( {planet_name: {'name': planet_name, 'rightascension': a[0]['ra_str'], 'distance': {'@errorplus': a[0]['st_disterr1'], '@errorminus': a[0]['st_disterr2']}, 'star': star, 'planet' : planet} } )
     
-    
-    catalog = { 'system' : {'name': planet_name, 'rightascension': a[0]['ra_str'], 'distance': {'@errorplus': a[0]['st_disterr1'], '@errorminus': a[0]['st_disterr2']}, 'star': star, 'planet' : planet} }
-    
-    print(catalog)
+    final_catalog.update({"System" : catalog})
+    print(final_catalog)
