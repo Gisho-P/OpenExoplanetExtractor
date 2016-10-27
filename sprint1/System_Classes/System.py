82c68184c0eb1557fbf54fe8195885961b78a273
venkatkorapaty <venkat.korapaty@mail.utoronto.ca>
2016-10-27 00:43:09 -0400

    created classes for each OEC system

--- sprint1/System_Classes/System.py
+++ sprint1/System_Classes/System.py
@@ -1,27 +1,25 @@
-import xmltodict
-import urllib
-import dicttoxml
 import collections
 import Star
 
 class Systems():
 
-    def __init__(self, url):
-    	data = urllib.request.urlopen(url).read().decode('utf-8')
+    def __init__(self, system_dict):
+	
+	self.system_dict = system_dict
 
-    	self.system_dict = xmltodict.parse(data, dict_constructor=dict)
-
-    	initialize_system_xml_values()
+    	initialize_system_dict_values()
 
     	self.stars = []
     	for star in self.system_dict['system']['star']:
-    		temp_star = Star(star)
-    		self.stars.append(temp_star)
+	    temp_star = Star(star)
+	    self.stars.append(temp_star)
 
 
-    def initialize_system_xml_values():
+    def initialize_system_dict_values():
     	self.name = self.system_dict['system']['name']
     	self.right_ascension = self.system_dict['system']['rightascension']
     	self.declination = self.system_dict['system']['declination']
-    	self.distance_error_minus = self.system_dict['system']['distance']['@errorminus']
-    	self.distance_error_plus = self.system_dict['system']['distance']['@errorplus']
+    	self.distance_error_minus = self.system_dict['system']['distance']
+	['@errorminus']
+    	self.distance_error_plus = self.system_dict['system']['distance']
+	['@errorplus']
