41315815f474facddafe237ec53ccf91111e11a8
Gisho <gisho.pushparajah@mail.utoronto.ca>
2016-11-03 19:06:56 -0400

    fixed eureader to store planets as a part of the star dict

--- src/test.py
+++ src/test.py
@@ -2,9 +2,10 @@ import xmltodict
 import urllib
 import dicttoxml
 import collections
+import exoplanetEUreader
 
 def readOEC():
-    url = "https://raw.githubusercontent.com/OpenExoplanetCatalogue/open_exoplanet_catalogue/master/systems/CoRoT-24.xml"
+    url = "https://raw.githubusercontent.com/OpenExoplanetCatalogue/open_exoplanet_catalogue/master/systems/Kepler-489.xml"
     data = urllib.request.urlopen(url).read().decode('utf-8')
     result = xmltodict.parse(data, dict_constructor=dict)
     return result
@@ -14,8 +15,10 @@ def dicttoXML(data):
     result = dicttoxml.dicttoxml(data, root=False, attr_type=False)
     return result
 
+def csvToDictToXML():
+    result = dicttoxml.dicttoxml(exoplanetEUreader.readExoplaneteu()[0], root=False, attr_type=False)
+    return result
 
-
-
-print(readOEC())
+#print(readOEC())
 print(dicttoXML(readOEC()))
+#print(csvToDictToXML())
