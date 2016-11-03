fbd29b9454ec4f1a720ef97d87ae64fa7af04ecc
venkatkorapaty <venkat.korapaty@mail.utoronto.ca>
2016-11-03 00:28:57 -0400

    got rid of unnecessary comments

--- src/test/system_class_tests.py
+++ src/test/system_class_tests.py
@@ -1,9 +1,7 @@
-# import xml.etree.ElementTree
 import unittest
 import urllib
 import dicttoxml
 import xmltodict
-# from path import path
 import sys
 sys.path.insert(0, '../system_classes')
 import system
@@ -11,9 +9,6 @@ import system
 class TestContained(unittest.TestCase):
 
     def test_hold_data(self):
-        # url = "https://raw.githubusercontent.com/OpenExoplanetCatalogue/ope" \
-        # "n_exoplanet_catalogue/master/systems/CoRoT-24.xml"
-        # data = urllib.request.urlopen(url).read().decode('utf-8')
         data = open('CoRoT-24.xml', 'r').read()
         result = xmltodict.parse(data, dict_constructor=dict)
         system = System(result)
