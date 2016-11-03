10e42f04e6251e9a850d42beca5c69bf4d039c9c
Venkat Korapaty <korapaty@IITS-I406-20.utsc-labs.utoronto.ca>
2016-11-03 19:11:03 -0400

    got second unit test working

--- src/test/system_class_tests.py
+++ src/test/system_class_tests.py
@@ -19,11 +19,15 @@ class TestContained(unittest.TestCase):
         system = System(self.system_dict)
         result = system.declination
         expected = "-03 43 10"
-        print(result)
         self.assertEqual(result, expected)
 
-    #def test_holds_different_planet_data(self):
-
+    def test_holds_different_planet_data(self):
+        system = System(self.system_dict)
+        result = system.stars.planets[0].mass_upper_limit == '0.018'
+        result = system.stars.planets[1].mass_upper_limit == ''
+        result = system.stars.planets[0].mass_error_minus == ''
+        result = system.stars.planets[1].mass_error_minus == '0.035'
+        self.assertTrue(result)
 
 if __name__ == '__main__':
     unittest.main(exit=False)
