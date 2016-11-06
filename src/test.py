a59f3a927c57a4d40cf1463aa3f31d0e4d1ac5e5
HarshilPatel12 <harshilp0@gmail.com>
2016-11-05 23:41:25 -0400

    Finds the file path of a given system

--- src/test.py
+++ src/test.py
@@ -2,7 +2,8 @@ import xmltodict
 import urllib
 import dicttoxml
 import collections
-import exoplanetEUreader
+import os
+import glob
 
 def readOEC():
     url = "https://raw.githubusercontent.com/OpenExoplanetCatalogue/open_exoplanet_catalogue/master/systems/Kepler-489.xml"
@@ -19,6 +20,32 @@ def csvToDictToXML():
     result = dicttoxml.dicttoxml(exoplanetEUreader.readExoplaneteu()[0], root=False, attr_type=False)
     return result
 
+def searchForNew(system_name):
+    # Search the directory for the name
+    fileList = os.listdir('./systems')
+    # Loop through and get rid of all the file extensions
+    path = " "
+    for name in fileList:
+        if(name[:len(name) -4] == system_name):
+            # Return the path of the file
+            path = "./systems/" + system_name + ".xml"
+            
+    return(path)
+        
+        
+    
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
 #print(readOEC())
-print(dicttoXML(readOEC()))
+#print(dicttoXML(readCatalogue()))
 #print(csvToDictToXML())
+print(searchForNew('11 Com'))
