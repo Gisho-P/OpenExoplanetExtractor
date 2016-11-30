cd277a534796879eb267c40052734515093a3a1a
BenaichaA <amine.benaicha@mail.utoronto.ca>
2016-11-29 23:50:09 -0500

    fixed implementation of git pull request

--- src/gitCommands.py
+++ src/gitCommands.py
@@ -1,8 +1,7 @@
 import subprocess
-import urllib.request
-import urllib.parse
-import http.client
-token = "a02f89cd8bca565ae600e30b1388bc57954e0a1a"
+import requests
+import json
+
 def gitPull():
 	p = subprocess.Popen(args=['./gitCommands.sh pull' ], shell=True)
 	p.wait()
@@ -32,26 +31,14 @@ def gitCheckout(branch_name):
 	p.wait()
 
 def gitPullRequest(branch_name):
-	url = "https://api.github.com/repos/T01test/open_exoplanet_catalogue/pulls/"
+	url = "https://api.github.com/repos/T01test/open_exoplanet_catalogue/pulls"
 	data = {
 	        'title': 'Merge %s with master'%(branch_name),
-	        'head': 'T01test:%s'%(branch_name),
+	        'head': '%s'%(branch_name),
 	        'base': 'master'
 	      }
-	#conn = http.client.HTTPSConnection("api.github.com")
-	#conn.putheader('Authorization', 'token a02f89cd8bca565ae600e30b1388bc57954e0a1a')
-	#conn.endheader()
-	#conn.putrequest("POST", "/repos/T01test/open_exoplanet_catalogue/pulls/", urllib.urlencode(data).encode())
-	#conn.send()
-	#response = conn.getresponse()
-	#print(response.status, response.reason)
-	#request = urllib.request.Request(url, urllib.parse.urlencode(data).encode())
-	#print(urllib.request.urlopen(request).read().decode())
 
-	request = urllib.request.Request(url, urllib.parse.urlencode(data).encode())
-	request.add_header('Authorization', 'token %s' % token)
-	response = urllib.request.urlopen(request)
-	print(response.read())
+	request = requests.post(url, json.dumps(data), auth=('T01test', 'passorfail1'))
 
 gitPullRequest("11-29-15-38")
 
